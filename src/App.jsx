0ca6f70da2a62233a3856d15eb107d58362e60eb
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2025-02-12 15:28:09 -0500

    initiate files & add Pokemon.js w/ pokemon array

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Pokedex />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Pokedex.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pokedex = ({ name, img, type, EXP }) => {
+  return;
+  <div className="pokecard">
+    {
+        name: {name}
+    }
+
+  </div>;
+};
